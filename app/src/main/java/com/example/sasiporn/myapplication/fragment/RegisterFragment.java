a31477ca6959d43e905b75368cfbb5c22493a709
Sasiporn <Sasiporn.409@gmail.com>
2017-11-07 12:02:26 +0700

    before Lunch

---
+++ app/src/main/java/com/example/sasiporn/myapplication/fragment/RegisterFragment.java
@@ -0,0 +1,24 @@
+package com.example.sasiporn.myapplication.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.sasiporn.myapplication.R;
+
+/**
+ * Created by sasiporn on 11/7/2017 AD.
+ */
+
+public class RegisterFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_register, container, false)
+        return view;
+    }
+}   // Main Class
