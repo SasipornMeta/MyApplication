806610346aa1a5c70329659d6673a87149f23d50
Sasiporn <Sasiporn.409@gmail.com>
2017-11-09 11:58:59 +0700

    crate toolbar OK

--- app/src/main/java/com/example/sasiporn/myapplication/fragment/ServiceFragment.java
+++ app/src/main/java/com/example/sasiporn/myapplication/fragment/ServiceFragment.java
@@ -3,11 +3,13 @@ package com.example.sasiporn.myapplication.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.sasiporn.myapplication.MyServiceActivity;
 import com.example.sasiporn.myapplication.R;
 
 /**
@@ -33,8 +35,16 @@ public class ServiceFragment extends Fragment{
         String[] strings = getArguments().getStringArray("Login");
         Log.d("9novV1", "Login(1) on ServiceFragment ==> " + strings[1]);
 
+//        Create Toolbar
+        createToolbar(strings[1]);
 
+    }
 
+    private void createToolbar(String strTitle) {
+        Toolbar toolbar = getView().findViewById(R.id.toolbarService);
+        ((MyServiceActivity)getActivity()).setSupportActionBar(toolbar);
+        ((MyServiceActivity) getActivity()).getSupportActionBar().setTitle(strTitle);
+        ((MyServiceActivity) getActivity()).getSupportActionBar().setSubtitle("Who Loged");
 
     }
 
